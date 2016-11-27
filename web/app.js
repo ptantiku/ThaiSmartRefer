5e77e5b15013df268bca66f258e86ce65815e8db
Phitchayaphong Tantikul <ptantiku@gmail.com>
2016-11-27 12:31:03 +0700

    storing cookie named "lastblock" and filter dashboard by lastblock

--- web/app.js
+++ web/app.js
@@ -1,15 +1,28 @@
 var express = require('express')
+var cookieParser = require('cookie-parser')
+
 var app = express()
+
+//use cookie
+app.use(cookieParser())
 
 //serving static files
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    res.render('index', {title: 'Hello World'})
+
+    var title = req.cookies.title;
+    res.cookie('title','test');
+    res.render('index', {title: title})
 })
 
 app.get('/dashboard', function (req, res) {
-    res.render('dashboard', {title: 'Hello World'})
+    var lastblock = 0;
+    if(req.cookies.lastblock) {
+        lastblock = req.cookies.lastblock;
+    }
+
+    res.render('dashboard', {title: 'Hello World', lastblock: lastblock})
 })
 
 app.set('views', './views')
